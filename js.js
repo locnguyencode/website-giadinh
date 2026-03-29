2849272a9f7c3699c29c79278ba311ad1f5c6e6e
locnguyenlaptrinh <locn0920@gmail.com>
2026-03-29 13:53:37 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+function bamvaoemdi(){
+document.getElementById("hello").innerHTML="bấm cái gì bỏ điện thoại xuống học đi👊✊"; 
+document.getElementById("hi").innerHTML="CÒN NGỒI ĐÓ LÀM GÌ BỎ CÁI ĐIỆN THOẠI RA COI👊✊"; 
+}
